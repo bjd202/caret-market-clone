a72545e811bfb5c146b9b0816bbdae548be28524
bjd202 <44849201+bjd202@users.noreply.github.com>
2022-07-20 23:01:05 +0900

    Update DrawList.js

--- client/src/component/DrawList.js
+++ client/src/component/DrawList.js
@@ -1,9 +1,15 @@
-import { AppBar, Avatar, Box, Grid, IconButton, List, ListItem, ListItemAvatar, ListItemText, Paper, Toolbar } from '@mui/material'
+import { AppBar, Avatar, Backdrop, Box, Card, CardContent, CardMedia, Divider, Grid, IconButton, List, ListItem, ListItemAvatar, ListItemText, Paper, SpeedDial, SpeedDialAction, SpeedDialIcon, Toolbar, Typography } from '@mui/material'
 import React from 'react'
 import SearchIcon from '@mui/icons-material/Search';
 import ImageIcon from '@mui/icons-material/Image';
 import WorkIcon from '@mui/icons-material/Work';
 import BeachAccessIcon from '@mui/icons-material/BeachAccess';
+import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
+import SaveIcon from '@mui/icons-material/Save';
+import PrintIcon from '@mui/icons-material/Print';
+import ShareIcon from '@mui/icons-material/Share';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
 function DrawList() {
   return (
@@ -22,38 +28,84 @@ function DrawList() {
           </Toolbar>
       </AppBar> 
       
-      <List sx={{ 
-        width: '100%',
-        bgcolor: 'background.paper' ,
-        paddingTop: '56px',
-      }}>
-        <ListItem>
-          <ListItemAvatar>
-            <Avatar>
-              <ImageIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="Photos" secondary="Jan 9, 2014" />
-        </ListItem>
-        <ListItem>
-          <ListItemAvatar>
-            <Avatar>
-              <WorkIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="Work" secondary="Jan 7, 2014" />
-        </ListItem>
-        <ListItem>
-          <ListItemAvatar>
-            <Avatar>
-              <BeachAccessIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="Vacation" secondary="July 20, 2014" />
-        </ListItem>
-      </List>
+      <Box sx={{paddingTop: '56px'}}>
+        <Card sx={{display: 'flex', padding: 2}}>
+          <CardMedia
+            component="img"
+            sx={{width: 150}}
+            image=""
+          >
+
+          </CardMedia>
+          
+          <Box sx={{display: 'flex', flexDirection: 'column', width: '100%'}}>
+            <CardContent>
+              <Typography component="div" variant='subtitle1'>
+                제목
+              </Typography>
+              <Typography component="div" variant='subtitle1' color="text.secondary">
+                주소 + 게시물 시간
+              </Typography>
+              <Typography component="div" variant='subtitle2' sx={{fontWeight: 'bold'}}>
+                가격
+              </Typography>
+              <Box sx={{display: 'flex', justifyContent: 'flex-end'}}>
+                <ChatBubbleOutlineIcon color='primary' />
+                <Typography>1</Typography>
+                <FavoriteBorderIcon color='error' />
+                <Typography>2</Typography>
+              </Box>
+            </CardContent>
+          </Box>
+        </Card>
+        
+      </Box>
+      
+
+      <ControlledOpenSpeedDial/>
     </Box>
   )
+}
+
+function ControlledOpenSpeedDial() {
+  const actions = [
+    { icon: <FileCopyIcon />, name: 'Copy' },
+    { icon: <SaveIcon />, name: 'Save' },
+    { icon: <PrintIcon />, name: 'Print' },
+    { icon: <ShareIcon />, name: 'Share' },
+  ];
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    if(open){
+      setOpen(false)
+    }else{
+      setOpen(true)
+    }
+  };
+  const handleClose = () => setOpen(false);
+
+  return (
+    // <Box sx={{ height: 600, transform: 'translateZ(0px)', flexGrow: 1 }}>
+    <Box>
+      <Backdrop open={open} />
+      <SpeedDial
+        ariaLabel="SpeedDial basic example"
+        sx={{ position: 'fixed', bottom: 80, right: 16 }}
+        icon={<SpeedDialIcon />}
+        onClick={handleOpen}
+        open={open}
+      >
+        {actions.map((action) => (
+          <SpeedDialAction
+            key={action.name}
+            icon={action.icon}
+            tooltipTitle={action.name}
+          />
+        ))}
+      </SpeedDial>
+   </Box>
+  );
 }
 
 export default DrawList
