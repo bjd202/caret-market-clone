59f8bfd4b40c69a5eb4dfc66a861505f55ed33c7
bjd202 <44849201+bjd202@users.noreply.github.com>
2022-07-24 04:25:49 +0900

    중고거래 글쓰기 화면 틀

--- client/src/component/CreatePost.js
+++ client/src/component/CreatePost.js
@@ -1,10 +1,11 @@
-import { AppBar, Box, Button, Divider, IconButton, TextField, Toolbar, Typography } from '@mui/material';
-import React from 'react'
+import { AppBar, Box, Button, Divider, FormControl, IconButton, InputLabel, MenuItem, Select, TextField, Toolbar, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import PhotoCamera from '@mui/icons-material/PhotoCamera';
 import SearchIcon from '@mui/icons-material/Search';
 import CloseIcon from '@mui/icons-material/Close';
 import Image from '@mui/icons-material/Image';
+import axios from 'axios';
 
 function CreatePost(props) {
   console.log(props);
@@ -17,13 +18,89 @@ function CreatePost(props) {
     navigate('/');
   }
 
+  const [images, setImages] = useState([])
+  const [subject, setSubject] = useState('');
+  const [category, setCategory] = useState('');
+  const [number, setNumber] = useState(0);
+
   const goList = () => {
     navigate('/list');
   }
 
-  const handleCreate = () => {
-
+  const handleSubject = (e) => {
+    const {id, value} = e.target
+    setSubject(value);
   }
+
+  const handleCategory = (e) => {
+    const {id, value} = e.target
+    setCategory(value);
+    console.log(value);
+  }
+
+  const handleNumber = (e) => {
+    const {id, value} = e.target
+    setNumber(value);
+  }
+
+  const handleImage = (e) => {
+    console.log(e.target.files);
+
+    if(images.length >= 10){
+      alert('이미지는 10개 이상 업로드 불가능합니다.');
+      return;
+    }
+
+    for (const file of e.target.files) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        setImages((image) => [...image, reader.result]);
+      }
+      reader.onerror = () => {
+        console.log(reader.error);
+      }
+    }
+
+    console.log(images);
+  }
+
+  const removeImage = (e) => {
+    const i = Number(e.target.getAttribute('data-key'));
+    console.log(i);
+
+    const tmpArr = images.filter((value, index) => {
+      console.log(index);
+      return i !== index;
+    })
+    console.log(tmpArr);
+
+    setImages(tmpArr);
+
+    // console.log(images);
+  }
+
+  const handleCreate = (e) => {
+    const data = {
+      subject,
+      category,
+      number
+    }
+
+    console.log(data);
+
+    axios.post('http://localhost/usedTrade/create', data)
+    .then(res => {
+
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <Box>
@@ -41,36 +118,78 @@ function CreatePost(props) {
         </Toolbar>
       </AppBar> 
 
-      <Box sx={{paddingTop: '56px'}}>
-        <Box sx={{display: 'flex'}}>
+      <Box component='form' noValidate sx={{paddingTop: '70px'}}>
+        <Box sx={{display: 'flex', flexWrap: 'nowrap'}}>
           <Box sx={{paddingTop: '20px', paddingBottom: '20px', display: 'inline-flex', flexDirection: 'column', flexWrap: 'nowrap', alignItems: 'center'}}>
             <IconButton sx={{border: '1px solid'}} color="primary" aria-label="upload picture" component="label">
-              <input hidden accept="image/*" type="file" />
+              <input 
+                id='images'
+                hidden 
+                accept="image/*" 
+                type="file" 
+                onChange={handleImage}
+                multiple
+              />
               <PhotoCamera />
             </IconButton>
-            <Typography>0/10</Typography>
+            <Typography>{images.length}/10</Typography>
           </Box>
 
-          <Box>
-            <img style={{width: '100px'}} src='https://images.unsplash.com/photo-1551963831-b3b1ca40c98e' />
+          <Box sx={{display: 'flex', flexWrap: 'wrap'}}>
+            {/* <img style={{width: '100px'}} src='https://images.unsplash.com/photo-1551963831-b3b1ca40c98e' /> */}
+            {images.map((value, index) => {
+              return (
+                <Box key={index} sx={{display: 'flex', alignItems: 'flex-start'}}>
+                  <img style={{width: '100px'}} src={value} />
+                  <IconButton data-key={index} onClick={removeImage}>
+                    <CloseIcon data-key={index} />
+                  </IconButton>
+                </Box>
+              )
+            })}
           </Box>
         </Box>
         <Divider />
 
         <Box sx={{paddingTop: '20px', paddingBottom: '20px'}}>
-          <TextField id="subject" label="글 제목" variant="outlined" fullWidth />
+          <TextField 
+            id="subject" 
+            label="글 제목" 
+            variant="outlined" 
+            fullWidth 
+            onChange={handleSubject}
+          />
         </Box>
         
         <Divider />
 
         <Box sx={{paddingTop: '20px', paddingBottom: '20px'}}>
-          <TextField id="subject" label="Outlined" variant="outlined" />
+          <FormControl fullWidth>
+            <InputLabel id="category">카테고리 선택</InputLabel>
+            <Select
+              labelId="category"
+              id="category"
+              label="카테고리 선택"
+              value={category || ''}
+              onChange={handleCategory}
+            >
+              <MenuItem value={'디지털기기'}>디지털기기</MenuItem>
+              <MenuItem value={'생활가전'}>생활가전</MenuItem>
+              <MenuItem value={'가구/인테리어'}>가구/인테리어</MenuItem>
+            </Select>
+          </FormControl>
         </Box>
 
         <Divider />
 
         <Box sx={{paddingTop: '20px', paddingBottom: '20px'}}>
-          <TextField id="subject" label="$ 가격 (선택사항)" variant="outlined" fullWidth />
+          <TextField 
+            type='number'
+            id="subject" 
+            label="$ 가격 (선택사항)" 
+            variant="outlined" 
+            onChange={handleNumber}
+            fullWidth />
         </Box>
       </Box>
       
